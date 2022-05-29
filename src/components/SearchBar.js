37eaf0facd0191b3639093ec0a79491820c0ad25
BRB3D <samuraix221@hotmail.com>
2022-05-28 23:37:44 -0400

    value update

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,7 +10,10 @@ state = { term: '' };
       <form className="ui form">
         <div className="field">
           <label>Image Search</label>
-          <input type="text" value={this.state.term} onChange={(e) => this.setState({ term: e.target.value})}></input>
+          <input type="text" 
+          value={this.state.term} 
+          onChange={(e) => this.setState({ term: e.target.value})}>
+          </input>
         </div>
       </form>
     </div>
