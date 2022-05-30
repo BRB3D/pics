b087bb7624e27115d1ef5e871cd5d61b67878e48
BRB3D <samuraix221@hotmail.com>
2022-05-29 23:14:13 -0400

    default value for axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
+  state = { images: [] }; 
+
+  async onSearchSubmit(term) {
+   const response = await unsplash.get('/search/photos', {
       params: {
         query: term
-      },
-      headers: {
-        Authorization: `Client-ID ${process.env.REACT_APP_Client_ID}`
       }
     })
-    .then((res) => {
-      console.log(res);
-    })
+
+    this.setState({ images: response.data.results });
   }
  
   render() {
    return( 
       <div className="ui container" style={{marginTop: '10px'}}>
-        <SearchBar onSubmit={this.onSearchSubmit}/>
+        <SearchBar onSubmit={(e) => this.onSearchSubmit(e)}/>
+        Found: {this.state.images.length} images
       </div>
     );
   };
